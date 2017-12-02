34c35fde466a004b099d2e3309743f4ca2df9aed
daniel <daniel@saakes.net>
2017-12-02 15:26:51 +0900

    OSC speed control in Unity

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 
 The unity projects in this repository include OSC code from thomasfredericks (see license):
 https://github.com/thomasfredericks/UnityOSC
+
+The Dutch flowerfield image
+https://commons.wikimedia.org/wiki/File:Dutch_flower_fields_(360_degree_aerial_pano).jpg
--- mobile_version/Unity_GVR_project/Assets/OSC/ReceiveSpeed.cs
+++ mobile_version/Unity_GVR_project/Assets/OSC/ReceiveSpeed.cs
@@ -4,16 +4,24 @@ using System.Collections;
 public class ReceiveSpeed : MonoBehaviour {
     
    	public OSC osc;
+	public float speed;
+
+	UnityEngine.Video.VideoPlayer mVideo;
 
 	// Use this for initialization
 	void Start () {
 	   osc.SetAddressHandler("/speed", OnReceiveSpeed);
+	   // connect to the video player
+	   mVideo = gameObject.GetComponent<UnityEngine.Video.VideoPlayer>();
+	   Debug.Log("start receive speed " );
+
     }
 	// Update is called once per frame
 	void Update () {
 	
 	}
-	void OnReceiveSpeed(OscMessage message){
-		float x = message.GetFloat(0);
+	void OnReceiveSpeed(OscMessage m){
+		speed = m.GetFloat(0);
+		mVideo.playbackSpeed = speed;
 	}
 }
